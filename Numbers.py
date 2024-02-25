f8070b24092410f9989b0f78d87e795b0732daa7
grrlofhighart <149973180+grrlofhighart@users.noreply.github.com>
2024-02-25 16:34:40 -0500

    Added pscode-exercises.txt
    Added pscode-exercises.txt

---
+++ Numbers.py
@@ -0,0 +1,26 @@
+#Ask user for input
+print("Please enter 5 numbers.")
+
+#Make list to hold numbers
+numbers=[]
+
+#Loop to collect 5 inputs
+for i in range(5):
+    try:
+        # Read input from the user
+        number = (int(input("Enter number: ")))
+        numbers.append(number)
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+
+#Calculate
+sum_list = sum(numbers)
+average = sum(numbers) / len(numbers)
+
+#Display results
+print('You entered:', (numbers))
+print(sorted(numbers[0:1]), 'is the smallest number in this list.')
+print(sorted(numbers[(len(numbers)-1):len(numbers)]), 'is the largest number in this list.')
+print((average), 'is the average of this list of numbers.')
+print(sum_list)
