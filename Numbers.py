2f7fc42536a75abceafe6f88fae6a7e8d32e16e3
grrlofhighart <149973180+grrlofhighart@users.noreply.github.com>
2024-02-25 20:10:30 -0500

    Updated Numbers.py
    Changed to While loop
    Added sort method

--- Numbers.py
+++ Numbers.py
@@ -5,22 +5,24 @@ print("Please enter 5 numbers.")
 numbers=[]
 
 #Loop to collect 5 inputs
-for i in range(5):
+i = 0
+while i < 5:
     try:
         # Read input from the user
         number = (int(input("Enter number: ")))
         numbers.append(number)
+        i = i + 1
+
     except ValueError:
         print("Invalid input. Please enter a valid number.")
-
 
 #Calculate
 sum_list = sum(numbers)
 average = sum(numbers) / len(numbers)
 
 #Display results
+numbers.sort()
 print('You entered:', (numbers))
 print(sorted(numbers[0:1]), 'is the smallest number in this list.')
 print(sorted(numbers[(len(numbers)-1):len(numbers)]), 'is the largest number in this list.')
 print((average), 'is the average of this list of numbers.')
-print(sum_list)
